b6383f527e120bfcf3ecd624549e7e8df96e009e
Gloria Ines Xiang-Yun Moudou <gloriamoudou@meta.com>
2024-06-28 16:50:07 -0700

    code cleanup

--- frontend/src/PlayersView/StatsTable.jsx
+++ frontend/src/PlayersView/StatsTable.jsx
@@ -14,7 +14,7 @@ function createData(
   field_goals,
   field_attempts,
   field_percent,
-  three_makes
+  three_makes,
 ) {
   return {
     player_name,
@@ -51,7 +51,7 @@ async function fetchPlayer(id) {
 
 async function handleFetchPlayers() {
   const response = await fetch(
-    "https://nba-stats-db.herokuapp.com/api/playerdata/season/2023"
+    "https://nba-stats-db.herokuapp.com/api/playerdata/season/2023",
   );
   const data = await response.json();
   const loadedPlayers = data.results;
