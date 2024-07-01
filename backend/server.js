16a45965ccbaba42bd7901910a0f2a19c5cbd124
Gloria Ines Xiang-Yun Moudou <gloriamoudou@meta.com>
2024-07-01 09:26:19 -0700

    run prettier again

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,30 @@
-import session from 'express-session';
-import cors from 'cors';
-import morgan from 'morgan';
-import userRoutes from './routes/users.js';
-import {PrismaClient} from '@prisma/client'
+import session from "express-session";
+import cors from "cors";
+import morgan from "morgan";
+import userRoutes from "./routes/users.js";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-import express from 'express'
+import express from "express";
 const app = express();
 
-import {} from 'dotenv/config'
+import {} from "dotenv/config";
 const PORT = process.env.PORT || 3000;
 const SECRET_KEY = process.env.SECRET_KEY;
 const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
-app.use(cors({
-  origin: `http://localhost:${FRONTEND_PORT}`,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: `http://localhost:${FRONTEND_PORT}`,
+    credentials: true,
+  }),
+);
 app.use(express.json());
-app.use(morgan())
+app.use(morgan());
 
 app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`
-));
+  console.log(`Server running on http://localhost:${PORT}`),
+);
 
 app.use(
   session({
@@ -32,52 +34,87 @@ app.use(
     cookie: {
       sameSite: false,
       secure: false,
-      expires: new Date(Date.now() + (30 * 24 * 60 * 60 * 1000))
-    }
-  })
+      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+    },
+  }),
 );
 
 app.use(userRoutes);
 
-app.post('/player', async (req, res) => {
-    const {AST, BLK, DRB, ORB, PF, PTS, STL, TOV, TRB, age,
-        effect_fg_percent, field_attempts, field_goals, field_percent,
-        ft, ft_percent, fta, games, games_started, id, minutes_played,
-        player_name, team, three_attempts, three_fg, three_percent,
-        two_attempts, two_fg, two_percent} = req.body;
+app.post("/player", async (req, res) => {
+  const {
+    AST,
+    BLK,
+    DRB,
+    ORB,
+    PF,
+    PTS,
+    STL,
+    TOV,
+    TRB,
+    age,
+    effect_fg_percent,
+    field_attempts,
+    field_goals,
+    field_percent,
+    ft,
+    ft_percent,
+    fta,
+    games,
+    games_started,
+    id,
+    minutes_played,
+    player_name,
+    team,
+    three_attempts,
+    three_fg,
+    three_percent,
+    two_attempts,
+    two_fg,
+    two_percent,
+  } = req.body;
 
-    const newPlayer = await prisma.player.create({
-        data: {
-            AST,
-            BLK,
-            DRB,
-            ORB,
-            PF,
-            PTS,
-            STL,
-            TOV,
-            TRB,
-            age,
-            effect_fg_percent,
-            field_attempts,
-            field_goals,
-            field_percent,
-            ft,
-            ft_percent,
-            fta,
-            games,
-            games_started,
-            id,
-            minutes_played,
-            player_name,
-            team,
-            three_attempts,
-            three_fg,
-            three_percent,
-            two_attempts,
-            two_fg,
-            two_percent,
-        }
-    });
-    res.json(newPlayer);
+  const newPlayer = await prisma.player.create({
+    data: {
+      AST,
+      BLK,
+      DRB,
+      ORB,
+      PF,
+      PTS,
+      STL,
+      TOV,
+      TRB,
+      age,
+      effect_fg_percent,
+      field_attempts,
+      field_goals,
+      field_percent,
+      ft,
+      ft_percent,
+      fta,
+      games,
+      games_started,
+      id,
+      minutes_played,
+      player_name,
+      team,
+      three_attempts,
+      three_fg,
+      three_percent,
+      two_attempts,
+      two_fg,
+      two_percent,
+    },
   });
+  res.json(newPlayer);
+});
+
+app.get("/recent", async (req, res) => {
+  const offset = new Date();
+  offset.setTime(Date.now() - 5 * 60 * 1000);
+  const board = await prisma.board.findMany({
+    where: { createdAt: { gte: offset } },
+  });
+  res.json(board);
+});
