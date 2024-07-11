11918d648b69449711cbe469f4a4e220eea74931
Gloria Ines Xiang-Yun Moudou <gloriamoudou@meta.com>
2024-07-11 11:06:57 -0700

    add more css

--- frontend/src/my_team/PlayerCard.jsx
+++ frontend/src/my_team/PlayerCard.jsx
@@ -3,12 +3,15 @@ import { useState } from "react";
 
 function PlayerCard({ player, setMyTeamPlayers, myTeamPlayers }) {
   const PORT = import.meta.env.VITE_BACKEND_PORT;
+  const [flipCard, setFlipCard] = useState(false);
 
-  function onDeleteClick() {
+  function onDeleteClick(event) {
+    event.stopPropagation();
     setDisplayDeleteModal(true);
   }
 
-  async function handleDeleteCard(e) {
+  async function handleDeleteCard(event) {
+    event.stopPropagation();
     setDisplayDeleteModal(false);
 
     const queryUrl = new URL(`${PORT}/singlePlayerStats`);
@@ -29,59 +32,76 @@ function PlayerCard({ player, setMyTeamPlayers, myTeamPlayers }) {
     );
   }
 
+  function handleFlipCard() {
+    setFlipCard(!flipCard);
+    setDisplayDeleteModal(false);
+  }
+
   const [displayDeleteModal, setDisplayDeleteModal] = useState(false);
   return (
-    <div className="playerCard">
-      <div className="cardHeader">
-        <h2 className="playerCardName"> {player.player_name}</h2>
-        <h1 className="performanceScore"> {player.performanceScore}</h1>
-      </div>
-
-      <div className="summaryStatsBar">
-        <h3 className="stat gamesPlayed">Games Played: {player.games}</h3>
-        <h3 className="stat pointsPerGame">
-          PPG: {player.games === 0 ? 0 : (player.PTS / player.games).toFixed(2)}
-        </h3>
-        <h3 className="stat fieldPercent">
-          Effect FG %: {(100 * parseFloat(player.field_percent)).toFixed(2)}
-        </h3>
-      </div>
-      <div className="hiddenStatsBar">
-        <div className="statRow">
-          <h3 className="stat fieldPercent">
-            Field %: {(100 * player.three_percent).toFixed(2)}
-          </h3>
-          <h3 className="stat threePercent">
-            Three %: {(100 * player.three_percent).toFixed(2)}
-          </h3>
-          <h3 className="stat twoPercent">
-            Two %: {(100 * player.two_percent).toFixed(2)}
-          </h3>
-          <h3 className="stat ftPercent">
-            FT %: {(100 * player.ft_percent).toFixed(2)}
-          </h3>
+    <div className="scene">
+      <div
+        className={`playerCard ${flipCard}`}
+        onClick={handleFlipCard}
+        style={{ transform: flipCard ? "rotateY(180deg)" : "" }}
+      >
+        <div className="card__face card__face--front">
+          <h2 className="playerCardName"> {player.player_name}</h2>
+          <h1 className="performanceScore"> {player.performanceScore}</h1>
         </div>
+        <div className="card__face card__face--back">
+          <div className="summaryStatsBar">
+            <h3 className="stat gamesPlayed">Games Played: {player.games}</h3>
+            <h3 className="stat pointsPerGame">
+              PPG:{" "}
+              {player.games === 0 ? 0 : (player.PTS / player.games).toFixed(2)}
+            </h3>
+            <h3 className="stat fieldPercent">
+              Effect FG %: {(100 * parseFloat(player.field_percent)).toFixed(2)}
+            </h3>
+          </div>
+          <div className="secondStatsRow">
+            <div className="statRow">
+              <h3 className="stat fieldPercent">
+                Field %: {(100 * player.three_percent).toFixed(2)}
+              </h3>
+              <h3 className="stat threePercent">
+                Three %: {(100 * player.three_percent).toFixed(2)}
+              </h3>
+              <h3 className="stat twoPercent">
+                Two %: {(100 * player.two_percent).toFixed(2)}
+              </h3>
+              <h3 className="stat ftPercent">
+                FT %: {(100 * player.ft_percent).toFixed(2)}
+              </h3>
+            </div>
 
-        <button className="deleteButton" onClick={onDeleteClick}>
-          Delete
-        </button>
-        <div
-          className="deleteCard"
-          style={{ display: displayDeleteModal ? "block" : "none" }}
-        >
-          <div className="deleteCardContent">
-            <p> Are you sure?</p>
-            <button
-              className="cancelDelete"
-              onClick={() => {
-                setDisplayDeleteModal(false);
-              }}
+            <button className="delete button" onClick={onDeleteClick}>
+              Delete
+            </button>
+            <div
+              className="deleteCard"
+              style={{ display: displayDeleteModal ? "block" : "none" }}
             >
-              No, Cancel
-            </button>
-            <button className="confirmDelete" onClick={handleDeleteCard}>
-              Yes, Delete
-            </button>
+              <div className="deleteCardContent">
+                <h2> Confirm Delete</h2>
+                <button
+                  className="button cancelDelete"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    setDisplayDeleteModal(false);
+                  }}
+                >
+                  No, Cancel
+                </button>
+                <button
+                  className="delete button confirmDelete"
+                  onClick={handleDeleteCard}
+                >
+                  Yes, Delete
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
